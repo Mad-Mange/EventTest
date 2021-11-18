1aec99938a83ac344d886ff46853350cf52d5904
Mad-Mange <madheli@hotmail.com>
2021-11-18 06:33:01 +0100

    Added some changes ;)

--- EventTestTaBort/Program.cs
+++ EventTestTaBort/Program.cs
@@ -3,40 +3,46 @@
     static void Main(string[] args)
     {
         BankStuff obj = new();
-        obj.GetKeyInput();
+        do
+        {
+            obj.GetKeyInput();
+        } while (obj.GetKeyInput());
     }
 }
 
 class BankStuff
 {
-    public enum keyValue { s, i, u }
-    public void GetKeyInput()
+    public event EventHandler MyEvent;
+    public enum keyValue { s, i, u, q }
+    public bool GetKeyInput()
     {
-        Console.Write("Ange vad du vill göra [s=SeSaldo,i=SättaIn, u=TaUt] : ");
+        Console.Write("Ange vad du vill göra [s=SeSaldo, i=SättaIn, u=TaUt, q=Quit] : ");
         var inKey = Console.ReadLine();
         int? sendKey = null;
-        if(inKey == "s") { sendKey = 1; } else if(inKey == "i") { sendKey = 2; } else if(inKey == "u") 
-                                    { sendKey = 3; } else { sendKey = null; } 
-        HandleKey(sendKey);
-        //Console.ReadLine();
+        if (inKey == "s") { sendKey = 1; }
+        else if (inKey == "i") { sendKey = 2; }
+        else if (inKey == "u") { sendKey = 3; }
+        else if (inKey == "q") { sendKey = 4; }
+        else{ sendKey = null; }
+        return HandleKey(sendKey);
     }
 
-    public void HandleKey(int? key)
+    public bool HandleKey(int? key)
     {
+        bool runAgain = true;
         switch (key)
         {
             case 1: Console.WriteLine("Du angav \"s\""); break;
             case 2: Console.WriteLine("Du angav \"i\""); break;
             case 3: Console.WriteLine("Du angav \"u\""); break;
-            default: Console.WriteLine("Du angav ogiltligt alternativ"); break;
+            case 4: Console.WriteLine("Du angav \"q\""); break;
+            default: Console.WriteLine("Du angav ogiltligt alternativ"); runAgain = false; break;
         }
+        return runAgain;
     }
 
     public void SeSaldo() { }
     public void SattaIn() { }
-    public void TaUt()
-    {
-        
-    }
+    public void TaUt() { }
 }
 
